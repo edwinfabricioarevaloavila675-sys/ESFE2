55ee6d73f2142fa3dba78ed8e2e1090bf262248b
Daniela Campos <danielaroblox1617@gmail.com>
2026-04-30 16:40:03 -0600

    Oscar Ramirez agrego las clases mostrados en los videos 7 y 8

---
+++ ESFE.BusinessLogic/BusinessLogicExtensions.cs
@@ -0,0 +1,26 @@
+﻿using ESFE.DataAccess;
+using Mapster;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ESFE.BusinessLogic
+{
+    public static class BusinessLogicExtensions
+    {
+        public static IServiceCollection AddBusinessLogicServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddMediatR(
+                cfg =>
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())
+            );
+            services.AddDataAccessServices(configuration);
+
+            TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
