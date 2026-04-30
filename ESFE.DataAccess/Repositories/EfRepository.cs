ee2597f2a28a2531017bd1267d7fadb476f8a375
Edwin Fabricio Arévalo Ávila <edwinfabricioarevaloavila675@gmail.com>
2026-04-30 14:00:34 -0600

    se creo archivos en interfaz

---
+++ ESFE.DataAccess/Repositories/EfRepository.cs
@@ -0,0 +1,54 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using ESFE.DataAccess.Interfaces;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.DataAccess.Repositories
+{
+    public class EfRepository<T> : RepositoryBase<T>, IEfRepository<T> where T : class
+    {
+        private readonly QuotationContext _context;
+        private IDbContextTransaction? _transaction;
+
+        public EfRepository(QuotationContext context) : base(context) 
+        {
+            _context = context;
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            if (_transaction != null)
+            {
+                return;
+            }
+
+            _transaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitAsync()
+        {
+            if (_transaction == null)
+            {
+                return;
+            }
+            await _context.SaveChangesAsync();
+            await _transaction.CommitAsync();
+            await _transaction.DisposeAsync();
+            _transaction = null;
+        }
+
+        public async Task RollbackAsync()
+        {
+            if (_transaction == null)
+            {
+                return;
+            }
+
+            await _transaction.RollbackAsync();
+            await _transaction.DisposeAsync();
+            _transaction = null;
+        }
+    }
+}
