aa891ba9fd4d0f2129f0e0ebfb96964e2435224a
Daniela Campos <danielaroblox1617@gmail.com>
2026-05-01 16:49:31 -0600

    Daniela Campos creó las Cotizaciones del video 23

---
+++ ESFE.BusinessLogic/DTOs/UserDto.cs
@@ -0,0 +1,24 @@
+﻿using ESFE.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.BusinessLogic.DTOs;
+
+public class UserResponse
+{
+    public int UserId { get; set; }
+
+    public int RolId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? UserNickname { get; set; }
+
+    public bool? UserStatus { get; set; }
+
+    public string? RolName { get; set; }
+}
+
+
+
