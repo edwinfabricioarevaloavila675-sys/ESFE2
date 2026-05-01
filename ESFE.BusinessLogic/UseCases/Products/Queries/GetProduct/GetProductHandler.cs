5aaa6bcacb09dd630c7343b6803c3e070d9852c0
Daniela Campos <danielaroblox1617@gmail.com>
2026-05-01 16:13:34 -0600

    Daniela Campos agregó nuevas clases para Obtener Producto por Id del video 22

---
+++ ESFE.BusinessLogic/UseCases/Products/Queries/GetProduct/GetProductHandler.cs
@@ -0,0 +1,26 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using ESFE.DataAccess.Interfaces;
+using ESFE.DataAccess.Repositories;
+using ESFE.Entities;
+using Mapster;
+using MediatR;
+
+namespace ESFE.BusinessLogic.UseCases.Products.Queries.GetProduct;
+
+internal sealed class GetProductHandler(IEfRepository<Product> repository) : IRequestHandler<GetProductQuery,ProductByIdResponse>
+{
+    public async Task<ProductByIdResponse> Handle(GetProductQuery query, CancellationToken cancellationToken)
+    {
+        
+        var product = await repository.GetByIdAsync(query.ProductId, cancellationToken);
+
+        
+        if (product is null)
+        {
+            return new ProductByIdResponse();
+        }
+
+        
+        return product.Adapt< ProductByIdResponse>();
+    }
+}
---
+++ ESFE.BusinessLogic/UseCases/Products/Queries/GetProduct/GetProductQuery.cs
@@ -0,0 +1,6 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using MediatR;
+
+namespace ESFE.BusinessLogic.UseCases.Products.Queries.GetProduct;
+
+public record GetProductQuery(long ProductId) : IRequest<ProductByIdResponse>;
