f6140319fe1ebb0a80bed79f924b0d21b24f0c43
Daniela Campos <danielaroblox1617@gmail.com>
2026-04-30 21:57:00 -0600

    Oscar Ramirez realizo las Clases del video numero 15

---
+++ ESFE.BusinessLogic/UseCases/Brands/Queries/GetBrands/GetBrandsHandler.cs
@@ -0,0 +1,28 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using ESFE.DataAccess.Interfaces;
+using ESFE.Entities;
+using Mapster;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE.BusinessLogic.UseCases.Brands.Queries.GetBrands;
+
+internal sealed class GetBrandsHandler(IEfRepository<Brand> _repository)
+    : IRequestHandler<GetBrandsQuery, List<BrandResponse>>
+{
+    public async Task<List<BrandResponse>> Handle(GetBrandsQuery query, CancellationToken cancellationToken)
+    {
+        var brands = await _repository.ListAsync(cancellationToken);
+
+        if (brands == null || !brands.Any())
+        {
+            return new List<BrandResponse>();
+        }
+
+        return brands.Adapt<List<BrandResponse>>();
+    }
+}
---
+++ ESFE.BusinessLogic/UseCases/Brands/Queries/GetBrands/GetBrandsQuery.cs
@@ -0,0 +1,6 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using MediatR;
+
+namespace ESFE.BusinessLogic.UseCases.Brands.Queries.GetBrands;
+
+public record GetBrandsQuery() : IRequest<List<BrandResponse>>;
